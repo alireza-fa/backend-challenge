4e5a1988dd94bd0a2f270a3a2232921fec5278fe
alireza-fa <alirezafeyze44@gmail.com>
2023-11-23 12:08:47 +0330

    add user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager as BaseManager
 
-# Create your models here.
+from apps.common.models import BaseModel
+
+
+class BaseUserManager(BaseManager):
+    def create_user(self, email, is_active=True, is_admin=False, password=None):
+        if not email:
+            raise ValueError('Users must have an email address.')
+
+        user = self.model(
+            email=self.normalize_email(email.lower()),
+            is_active=is_active, is_admin=is_admin)
+        if password is not None:
+            user.set_password(password)
+        else:
+            user.set_unusable_password()
+
+        user.full_clean()
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            is_active=True,
+            is_admin=True,
+            password=password
+        )
+
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class BaseUser(BaseModel, AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name='email address', unique=True)
+
+    is_active = models.BooleanField(default=True, verbose_name='is active')
+    is_admin = models.BooleanField(default=False, verbose_name='is admin')
+
+    objects = BaseUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+
+    def is_staff(self):
+        return self.is_admin
